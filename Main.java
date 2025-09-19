266dd43f897770d6d6b56924257754cfbfa4640e
EmonE47 <mdahsanulislamemon@gmail.com>
2025-09-19 21:22:18 +0600

    2nd One

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
@@ -5,5 +7,11 @@ public class Main {
         obj1.display();
         demo_class2 obj2 = new demo_class2();
         obj2.show();
+        System.out.println("Emon vai");
+        Scanner myScanner = new Scanner(System.in);
+        String name = myScanner.nextLine();
+        String greeting = myScanner.nextLine();
+        System.out.println("Your name is: " + name + greeting);
+        myScanner.close();
     }
 }
